f18c49c4944bb5fc0d997a1ce104bfa0796b96ff
yeimyhs <helihuancasancho@gmail.com>
2025-03-13 10:50:44 -0500

    errror de tipeo

--- SGI/settings.py
+++ SGI/settings.py
@@ -201,5 +201,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # para archivos estáticos adicionales
 ]
 
-MEDIA_URL = '/api/media/'  # URL pública para acceder a los archivos
+MEDIA_URL = '/media/'  # URL pública para acceder a los archivos
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Ruta donde se guardan los archivos físicamente
